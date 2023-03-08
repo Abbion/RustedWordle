c450a0cbc4ed0eec3bd53b6e2e3e102ebce62322
Abbion <amadeuszwalag@gmail.com>
2023-03-08 08:29:39 +0100

    Word cmp

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 
 use std::io::{BufReader, BufRead};
-use std::io;
+//use std::io;
 use std::fs::File;
 use rand::Rng;
+use colored::*;
+
+#[derive(Debug)]
+enum LetterState {
+    NoHit,
+    Hit,
+    Fit,
+}
 
 fn main() {
     println!("Welcome to rust wordle!");
@@ -16,9 +24,65 @@ fn main() {
 
     println!("Random wordle word {}", random_word);
 
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Failed to read input");
-    println!("You entered {}", input);
+    //let mut input = String::new();
+    //io::stdin().read_line(&mut input).expect("Failed to read input");
+    //println!("You entered {}", input);
+
+
+    let str1 = String::from("recog");
+    let str2 = String::from("regal");
+
+    let letter_states = check_user_word(&str1, &str2);
+    
+    for state in &letter_states{
+        println!("{:?}", state);
+    }
+
+    print_user_guess(&str1, &letter_states);
+
+}
+
+fn print_user_guess(user_str: &String, word_state: &Vec<LetterState>) {
+    for (i, letter) in user_str.chars().enumerate() {
+        
+    }
+
+    println!("This is green {}.", user_str.on_truecolor(83, 141, 78));
+    println!("This is yello {}.", user_str.on_truecolor(231, 184, 52));
+    println!("This is gray {}.", user_str.on_truecolor(109, 109, 109));
+}
+
+fn check_user_word(user_str: &String, word_str: &String) -> Vec<LetterState> {
+    let mut word_state = vec![LetterState::NoHit, LetterState::NoHit, LetterState::NoHit, LetterState::NoHit, LetterState::NoHit];
+    let mut checked_letters_vec = word_str.as_bytes().to_vec();
+
+    //Check for hit(higher priority)
+    for (i, &letter_u) in user_str.as_bytes().iter().enumerate() {
+        for (j, _) in word_str.as_bytes().iter().enumerate() {
+            if letter_u != checked_letters_vec[j] {
+                continue;
+            }
+            else if i == j {
+                word_state[i] = LetterState::Hit;
+                checked_letters_vec[j] = b'+';
+            }
+        }
+    }
+
+    //Check for fit
+    for (i, &letter_u) in user_str.as_bytes().iter().enumerate() {
+        for (j, _) in word_str.as_bytes().iter().enumerate() {
+            if letter_u != checked_letters_vec[j] {
+                continue;
+            }
+            else if i != j {
+                word_state[i] = LetterState::Fit;
+                checked_letters_vec[j] = b'+';
+            }
+        }
+    }
+
+    return  word_state;
 }
 
 fn load_random_word_from_list() -> Option<String> {
